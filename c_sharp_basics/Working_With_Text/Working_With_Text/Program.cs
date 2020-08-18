fe015c217fdde52de80ac46f7fa4d31c2151bc89
Ryan Waszak <ryanwaszak@gmail.com>
2020-08-18 12:43:48 -0700

    Add 64.Demo:String

--- c_sharp_basics/Working_With_Text/Working_With_Text/Program.cs
+++ c_sharp_basics/Working_With_Text/Working_With_Text/Program.cs
@@ -6,7 +6,36 @@ namespace Working_With_Text
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //64.Demo: String
+
+            var fullName = "Ryan Waszak ";
+            Console.WriteLine("Trim : '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper : '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine($"First name: {firstName}");
+            Console.WriteLine($" Last name: {lastName}");
+
+            var names = fullName.Split(' ');
+            Console.WriteLine($"First name: {names[0]}");
+            Console.WriteLine($" Last name: {names[1]}");
+
+            var newName = fullName.Replace("Ryan", "Ryan David");
+            Console.WriteLine(newName);
+
+            if (String.IsNullOrWhiteSpace(""))
+                Console.WriteLine("INVALID");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0"));
+
+            
         }
     }
 }
