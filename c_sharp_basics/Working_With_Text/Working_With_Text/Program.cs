03d7b57e2c310f5fb557a474b6a3bc63034f8a91
Ryan Waszak <ryanwaszak@gmail.com>
2020-08-19 10:04:33 -0700

    Finish Working_With_Text

--- c_sharp_basics/Working_With_Text/Working_With_Text/Program.cs
+++ c_sharp_basics/Working_With_Text/Working_With_Text/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Working_With_Text
 {
@@ -8,34 +10,64 @@ namespace Working_With_Text
         {
             //64.Demo: String
 
-            var fullName = "Ryan Waszak ";
-            Console.WriteLine("Trim : '{0}'", fullName.Trim());
-            Console.WriteLine("ToUpper : '{0}'", fullName.Trim().ToUpper());
+            //var fullName = "Ryan Waszak ";
+            //Console.WriteLine("Trim : '{0}'", fullName.Trim());
+            //Console.WriteLine("ToUpper : '{0}'", fullName.Trim().ToUpper());
 
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            Console.WriteLine($"First name: {firstName}");
-            Console.WriteLine($" Last name: {lastName}");
+            //var index = fullName.IndexOf(' ');
+            //var firstName = fullName.Substring(0, index);
+            //var lastName = fullName.Substring(index + 1);
+            //Console.WriteLine($"First name: {firstName}");
+            //Console.WriteLine($" Last name: {lastName}");
 
-            var names = fullName.Split(' ');
-            Console.WriteLine($"First name: {names[0]}");
-            Console.WriteLine($" Last name: {names[1]}");
+            //var names = fullName.Split(' ');
+            //Console.WriteLine($"First name: {names[0]}");
+            //Console.WriteLine($" Last name: {names[1]}");
 
-            var newName = fullName.Replace("Ryan", "Ryan David");
-            Console.WriteLine(newName);
+            //var newName = fullName.Replace("Ryan", "Ryan David");
+            //Console.WriteLine(newName);
 
-            if (String.IsNullOrWhiteSpace(""))
-                Console.WriteLine("INVALID");
+            //if (String.IsNullOrWhiteSpace(""))
+            //    Console.WriteLine("INVALID");
 
-            var str = "25";
-            var age = Convert.ToByte(str);
-            Console.WriteLine(age);
+            //var str = "25";
+            //var age = Convert.ToByte(str);
+            //Console.WriteLine(age);
 
-            float price = 29.95f;
-            Console.WriteLine(price.ToString("C0"));
+            //float price = 29.95f;
+            //Console.WriteLine(price.ToString("C0"));
 
-            
+            //##########################################################################################
+
+            //65.Live Coding: Summarising Text
+
+            //var sentence = "This is going to be a really really really really really long text.";
+            //var summary = StringUtility.SummerizeText(sentence, 25);
+            //Console.WriteLine(summary);
+
+
+            //##########################################################################################
+            //66. Demo: StringBuilder
+
+            var builder = new StringBuilder("Hello World");
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Char: " + builder[0]);
+            //##########################################################################################
+
+
         }
+
+
     }
 }
---
+++ c_sharp_basics/Working_With_Text/Working_With_Text/StringUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Working_With_Text
+{
+    public class StringUtility
+    {
+        public static string SummerizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+                return text;
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
+            }
+
+            return String.Join(' ', summaryWords) + "...";
+        }
+    }
+}
